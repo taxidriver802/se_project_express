7a7b52f5c0a5ef7bb59ba71befbe03aba95aef66
jason cox <JAcox12@icloud.com>
2025-04-15 22:41:40 -0500

    fixed unauthorized error class

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { UnauthorizedError } = require("../utils/constants");
+const { statusUnauthorized } = require("../utils/constants");
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return next(new UnauthorizedError("Authorization required"));
+    return next(new statusUnauthorized("Authorization required"));
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -16,7 +16,7 @@ const auth = (req, res, next) => {
     req.user = payload;
     next();
   } catch (err) {
-    next(new UnauthorizedError("Authorization required"));
+    next(new statusUnauthorized("Authorization required"));
   }
 };
 
