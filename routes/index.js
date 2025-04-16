6d65aedaf098bd58e21a45f0dfe62a71b4c19183
jason cox <JAcox12@icloud.com>
2025-04-16 00:05:43 -0500

    fix auth error

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const itemsRouter = require("./clothingItem");
 const usersRouter = require("./users");
 const { login, createUser } = require("../controllers/users");
-
+const auth = require("../middlewares/auth");
 const { statusNotFound } = require("../utils/constants");
 
 const router = express.Router();
@@ -12,8 +12,8 @@ router.post("/signin", login);
 router.post("/signup", createUser);
 
 // Routes
-router.use("/items", itemsRouter);
-router.use("/users", usersRouter);
+router.use("/items", auth, itemsRouter);
+router.use("/users", auth, usersRouter);
 
 router.use((req, res) => {
   const notFoundError = new statusNotFound();
